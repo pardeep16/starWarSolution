43fc1f39a2866afa5ae1d2f6a4404e2748e07c44
pardeep <pk.raswant@gmail.com>
2017-08-29 16:18:50 +0530

    update bugs in view

--- searchPatternsApis/searchCharactersFromAPI.js
+++ searchPatternsApis/searchCharactersFromAPI.js
@@ -346,7 +346,7 @@ var findPossiblePatterns=function(query,cb){
 	var pattern=query.toLowerCase().trim();
 	findPattern(pattern,function(err,result){
 		if(err){
-			cb({"status":false,"data":[]},null);
+			cb(null,{"status":false,"data":[]});
 			
 		}
 		else{
@@ -440,11 +440,11 @@ function getResponseFromAPI(pattern,data,cb){
 	else{
 		/*cb("No Result found",null);*/
 		//console.log(jsonData);
-		var nextUrl=jsonData.next.toString();
+		
 		//console.log("next url :"+nextUrl);
 		if(nextUrl!=null){
 			//console.log("Next Url:"+jsonData.next);
-			
+			var nextUrl=jsonData.next.toString();
 			requestCharApi(nextUrl,function(err,result){
 				if(err){
 					cb(err,null);
